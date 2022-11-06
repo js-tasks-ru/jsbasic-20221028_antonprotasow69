60f3ad9e48f2f9344304a7ca5d0233d75af1f40b
Anton Protasov <antonprotasow69@gmail.com>
2022-11-06 14:33:17 +0100

    сделал усечение строки

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,9 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  if (str.length > maxlength) {
+    let newString = str.slice(0, maxlength - 1) + '…';
+
+    return newString;
+  }
+
+  else return str;
 }
