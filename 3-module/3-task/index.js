288559f2e702204a080544fc1a9d690f85f19b7e
Anton Protasov <antonprotasow69@gmail.com>
2022-11-13 17:14:31 +0100

    sdelal 3 module 3 task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,13 @@
 function camelize(str) {
-  // ваш код...
+  let arr = str.split('-');
+  let newArr = arr.map(function (item, index) {
+    if (index != 0) {
+      return item[0].toUpperCase() + item.slice(1);
+    }
+    else
+      return item;
+  });
+  let result = newArr.join('');
+
+  return result;
 }
