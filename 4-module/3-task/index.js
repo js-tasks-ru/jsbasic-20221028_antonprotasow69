16a8db6456671c43d808bc98e84e59cafafa2ef2
Anton Protasov <antonprotasow69@gmail.com>
2022-11-17 16:14:47 +0100

    dodelal highlight task

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -5,7 +5,6 @@ function highlight(table) {
 
     for (let k = 0; k < row[i].cells.length; k++) {
 
-
       if (row[i].cells[k].hasAttribute("data-available")
         && row[i].cells[k].getAttribute("data-available") == 'true') {
 
@@ -15,25 +14,28 @@ function highlight(table) {
 
       else if (row[i].cells[k].hasAttribute("data-available")
         && row[i].cells[k].getAttribute("data-available") == 'false') {
+
         row[i].classList.add('unavailable');
 
       }
       else if (!row[i].cells[row[i].cells.length - 1].hasAttribute("data-available")) {
         row[i].setAttribute('hidden', '')
       }
+
       if (row[i].cells[k].innerHTML == 'm') {
         row[i].classList.add('male');
       }
+
       if (row[i].cells[k].innerHTML == 'f') {
         row[i].classList.add('female');
       }
+
       if (row[i].cells[k].innerHTML < 18) {
         row[i].style.textDecoration = 'line-through';
       }
+
     }
 
   }
-
-
 
 }
