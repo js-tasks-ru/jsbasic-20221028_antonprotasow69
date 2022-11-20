7cab9b2e39dd46ec53091422565929b8f88a67cd
Anton Protasov <antonprotasow69@gmail.com>
2022-11-20 18:46:51 +0100

    сделал скрытие/открытие по нажатию кнопки

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,11 @@
 function toggleText() {
-  // ваш код...
+  let elemButton = document.getElementsByClassName('toggle-text-button')[0];
+
+  function hide() {
+    text.hidden = !text.hidden;
+
+  }
+
+  elemButton.addEventListener('click', hide);
+
 }
