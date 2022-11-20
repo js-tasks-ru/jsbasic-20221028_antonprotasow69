5acb2d0fcb38ed85b111781d311c59ca4ac51757
Anton Protasov <antonprotasow69@gmail.com>
2022-11-21 00:39:39 +0100

    сделал часть карусели, подправил ссылки

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,30 @@
 function initCarousel() {
-  // ваш код...
+  let carousel = document.querySelector('.carousel');
+  let slides = document.getElementsByClassName('carousel__slide');
+  let width = slides[0].offsetWidth + 'px';
+
+  function slide(event) {
+    let target = event.target;
+
+    if (!target.classList.contains('carousel__arrow')) {
+      return;
+    }
+
+    else if (target.classList.contains('carousel__arrow_right')) {
+
+      slides[0].style.transform = `translateX( ${'-' + width} )`;
+
+
+    }
+
+    else if (target.classList.contains('carousel__arrow_left')) {
+      slides[0].style.transform = `translateX( ${width} )`;
+
+    }
+
+
+  }
+
+  carousel.addEventListener('click', slide)
+
 }
