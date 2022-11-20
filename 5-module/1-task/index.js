51037d86b748da57b044c5bb69c97b94f4786536
Anton Protasov <antonprotasow69@gmail.com>
2022-11-20 16:54:13 +0100

    сделал задание спрятать себя

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,10 @@
 function hideSelf() {
-  // ваш код...
+  let s = document.querySelector(".hide-self-button");
+
+  function hide1() {
+    s.setAttribute('hidden', '');
+  }
+
+  s.addEventListener('click', hide1);
+
 }
