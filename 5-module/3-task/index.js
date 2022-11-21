29b81e8610a44b4b5540a88fdf5b2aac366a964d
Anton Protasov <antonprotasow69@gmail.com>
2022-11-21 23:21:27 +0100

    подправил карусель

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -4,6 +4,7 @@ function initCarousel() {
   let slides = document.getElementsByClassName('carousel__inner');
   let arrowRight = document.querySelector('.carousel__arrow_right');
   let arrowLeft = document.querySelector('.carousel__arrow_left');
+  let arrows = document.querySelectorAll('.carousel__arrow > img');
   let width = slides[0].offsetWidth;
   let counter = 0;
 
@@ -13,40 +14,35 @@ function initCarousel() {
 
     let target = event.target;
 
-    if (!target.classList.contains('carousel__arrow')) {
+    if (target != arrows[0] && target != arrows[1] && target != arrowRight
+      && target != arrowLeft) {
       return;
 
     }
 
-    else if (target.classList.contains('carousel__arrow_right')) {
+    else if (target == arrows[0] || target == arrowRight) {
 
       counter += width;
       slides[0].style.transform = `translateX( ${'-' + counter + 'px'} )`;
 
       if (counter == width * 3) {
-
         arrowRight.style.display = 'none';
-
       }
-
     }
 
-    else if (target.classList.contains('carousel__arrow_left')) {
+    else if (target == arrows[1] || target == arrowLeft) {
+
       counter = counter - width;
       slides[0].style.transform = `translateX( ${'-' + counter + 'px'})`;
       arrowRight.style.display = '';
-
-
-
-
     }
+
     if (counter != 0) {
       arrowLeft.style.display = '';
     }
-    else arrowLeft.style.display = 'none';
 
+    else arrowLeft.style.display = 'none';
   }
 
   carousel.addEventListener('click', slide)
-
 }
