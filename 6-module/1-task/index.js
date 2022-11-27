0387342625c9da54e2c550017843b935c2a09fba
Anton Protasov <antonprotasow69@gmail.com>
2022-11-27 14:47:23 +0100

    sdelal таблицу с удаляемыми строками

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,18 +1,37 @@
-/**
- * Компонент, который реализует таблицу
- * с возможностью удаления строк
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
- *          name: 'Ilia',
- *          age: 25,
- *          salary: '1000',
- *          city: 'Petrozavodsk'
- *      }
- *
- */
 export default class UserTable {
+
+
   constructor(rows) {
+    this.rows = rows;
+    this.render();
   }
+
+  render() {
+    this.elem = document.createElement('table');
+    this.elem.innerHTML = this.rows.map(function (item) {
+      return `<tr>
+      <td> ${item.name} </td>
+      <td> ${item.age} </td>
+      <td> ${item.salary} </td>
+      <td> ${item.city} </td>
+      <td> <button> X </button> </td>
+      </tr>`
+
+    });
+
+    this.elem.addEventListener('click', this.remove);
+    return this.elem;
+  }
+  remove(ev) {
+    let target = ev.target;
+
+    if (target.closest('button')) {
+      target.closest('tr').remove();
+    }
+  }
+
+
 }
+
+
+
