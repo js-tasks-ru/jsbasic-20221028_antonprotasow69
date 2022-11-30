444564133184a93bdd28fb656a66ee0c1fb06a81
Anton Protasov <antonprotasow69@gmail.com>
2022-12-01 00:59:49 +0100

    оптимизировал модальное окно

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -2,47 +2,37 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
   constructor() {
-    this.documentBody = document.querySelector('body');
-    this.container = document.querySelector('.container');
+
     this.modalRender();
 
   }
 
   open() {
-    this.documentBody.classList.add('is-modal-open');
+    document.body.classList.add('is-modal-open');
 
   }
 
   modalRender() {
-    let elem = document.createElement('div');
-    elem.classList.add('modal');
 
-    this.elem = elem;
+    this.elem = createElement(`<div class="modal">
+    <div class="modal__overlay"></div>
+    <div class="modal__inner">
+    <div class="modal__header">
+      <!--Кнопка закрытия модального окна-->
+      <button type="button" class="modal__close">
+        <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+      </button>
+      <h3 class="modal__title">
+      Вот сюда нужно добавлять заголовок 
+      </h3>
+    </div>
+    <div class="modal__body">
+    A сюда нужно добавлять содержимое тела модального окна
+    </div>
+  </div>
+  </div>`)
 
-    let overlayModal = createElement(`
-    <div class="modal__overlay"></div>`);
-
-    let modalInner = createElement(`<div class="modal__inner">
-          <div class="modal__header">
-            <!--Кнопка закрытия модального окна-->
-            <button type="button" class="modal__close">
-              <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
-            </button>
-            <h3 class="modal__title">
-            Вот сюда нужно добавлять заголовок 
-            </h3>
-          </div>
-          <div class="modal__body">
-          A сюда нужно добавлять содержимое тела модального окна
-          </div>
-        </div>`)
-    this.elem.appendChild(overlayModal);
-    this.elem.appendChild(modalInner);
-
-
-    this.documentBody.append(this.elem);
-
-
+    document.body.appendChild(this.elem);
     this.closeOnMark();
     this.closeOnKey();
   }
@@ -65,7 +55,7 @@ export default class Modal {
   close() {
 
     this.elem.remove();
-    this.documentBody.classList.remove('is-modal-open');
+    document.body.classList.remove('is-modal-open');
 
     document.removeEventListener('keydown', this.escPressed)
 
@@ -78,7 +68,7 @@ export default class Modal {
     closeButton.addEventListener('click', () => {
 
       this.elem.remove()
-      this.documentBody.classList.remove('is-modal-open');
+      document.body.classList.remove('is-modal-open');
       document.removeEventListener('keydown', this.escPressed)
 
     });
