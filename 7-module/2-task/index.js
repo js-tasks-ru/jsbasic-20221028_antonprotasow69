aeb6d8042aa9bb984061d80f0e29d383a3fde4b5
Anton Protasov <antonprotasow69@gmail.com>
2022-12-01 00:39:03 +0100

    переделал модальное окно

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -2,97 +2,97 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
   constructor() {
-
-    this.closeOnMark();
-    this.closeOnKey();
-
+    this.documentBody = document.querySelector('body');
+    this.container = document.querySelector('.container');
+    this.modalRender();
 
   }
 
   open() {
+    this.documentBody.classList.add('is-modal-open');
 
-    this.elem = document.body;
-    this.elem.innerHTML = `<!--Корневой элемент Modal-->
-      <div class="modal">
-        <!--Прозрачная подложка перекрывающая интерфейс-->
-        <div class="modal__overlay"></div>
-    
-        <div class="modal__inner">
+  }
+
+  modalRender() {
+    let elem = document.createElement('div');
+    elem.classList.add('modal');
+
+    this.elem = elem;
+
+    let overlayModal = createElement(`
+    <div class="modal__overlay"></div>`);
+
+    let modalInner = createElement(`<div class="modal__inner">
           <div class="modal__header">
             <!--Кнопка закрытия модального окна-->
             <button type="button" class="modal__close">
               <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
             </button>
-
             <h3 class="modal__title">
             Вот сюда нужно добавлять заголовок 
             </h3>
           </div>
-    
           <div class="modal__body">
           A сюда нужно добавлять содержимое тела модального окна
           </div>
-        </div>
-    
-      </div>`;
-    let modalOpen = document.body;
-    modalOpen.classList.add('is-modal-open');
+        </div>`)
+    this.elem.appendChild(overlayModal);
+    this.elem.appendChild(modalInner);
 
+
+    this.documentBody.append(this.elem);
+
+
+    this.closeOnMark();
+    this.closeOnKey();
   }
 
   setTitle(titleName) {
-    // this.open();
-    let modalTitle = this.elem.querySelector('.modal__title');
 
+    let modalTitle = this.elem.querySelector('.modal__title');
     modalTitle.innerHTML = titleName;
   }
 
   setBody(modalBody) {
-    // this.open();
+
     let oldBody = this.elem.querySelector('.modal__body');
     oldBody.innerHTML = null;
 
-    let bodyModal = oldBody;
-    bodyModal.innerHTML = modalBody;
+    oldBody.appendChild(modalBody)
 
   }
 
   close() {
 
-    document.body.classList.remove('is-modal-open');
-    document.body.innerHTML = null;
+    this.elem.remove();
+    this.documentBody.classList.remove('is-modal-open');
+
     document.removeEventListener('keydown', this.escPressed)
 
   }
 
   closeOnMark() {
-    this.open()
 
+    let closeButton = this.elem.querySelector('.modal__close');
 
-    document.addEventListener('click', function (event) {
-      let target = event.target;
+    closeButton.addEventListener('click', () => {
 
-      if (target.closest('.modal__close')) {
+      this.elem.remove()
+      this.documentBody.classList.remove('is-modal-open');
+      document.removeEventListener('keydown', this.escPressed)
 
-        document.body.innerHTML = null;
-
-        document.body.classList.remove('is-modal-open');
-        document.removeEventListener('keydown', this.escPressed)
-      }
     });
+
   }
 
   closeOnKey() {
-
-    document.addEventListener('keydown', escPressed)
-
-    function escPressed(event) {
-
-      if (event.code === 'Escape') {
-        document.body.innerHTML = null;
-        document.body.classList.remove('is-modal-open');
-        document.removeEventListener('keydown', this.escPressed)
-      }
-    };
+    document.addEventListener('keydown', this.escPressed)
   }
+
+  escPressed = (event) => {
+
+    if (event.code === 'Escape') {
+      this.close();
+    };
+  };
 }
