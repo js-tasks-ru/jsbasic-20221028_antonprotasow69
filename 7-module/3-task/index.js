53b7074212a6e344dca1eb8de6e385184213eab6
Anton Protasov <antonprotasow69@gmail.com>
2022-12-01 19:24:25 +0100

    сделал первую часть слайдера

--- 7-module/3-task/index.js
+++ 7-module/3-task/index.js
@@ -1,4 +1,88 @@
 export default class StepSlider {
   constructor({ steps, value = 0 }) {
+    this.steps = steps;
+    this.value = value;
+    this.renderStepSlide();
+    this.sliderChangeValue()
   }
+
+  renderStepSlide() {
+
+    this.elem = document.createElement('div');
+    this.elem.classList.add('slider');
+    this.elem.innerHTML = `
+    <!--Ползунок слайдера с активным значением-->
+    <div class="slider__thumb">
+      <span class="slider__value">${this.value}</span>
+    </div>
+  
+    <!--Полоска слайдера-->
+    <div class="slider__progress"></div>
+  
+    <!-- Шаги слайдера (вертикальные чёрточки) -->
+    <div class="slider__steps">
+      
+    </div>`
+
+    let sliderSteps = this.elem.querySelector('.slider__steps');
+    //  <span class="slider__step-active"></span> - выделенный
+
+    for (let i = 0; i < this.steps; i++) {
+
+      let s = document.createElement('span');
+
+      if (i === 0) {
+        s.classList.add('slider__step-active');
+      }
+      sliderSteps.append(s);
+
+    }
+
+  }
+
+  sliderChangeValue() {
+
+    let slider = this.elem;
+    let allSteps = this.elem.querySelectorAll('.slider__steps > span');
+    let segmentSlider = allSteps.length - 1;
+    let sliderValue = this.elem.querySelector('.slider__value')
+
+    this.elem.querySelector('.slider__progress').style.width = '0%';
+
+    slider.addEventListener('click', (event) => {
+
+      let rect = slider.getBoundingClientRect(); // значения слайдера
+      let leftPercents = event.clientX - rect.left; // координаты курсора - расстояние от окноа до слайдера слева
+      let leftRelative = leftPercents / slider.offsetWidth; // относительное значение от слайдера
+      let approximateValue = leftRelative * segmentSlider; // относ. значение на кол-во сегментов
+      let value = Math.round(approximateValue); // округление значения
+
+      let valuePercents = value / segmentSlider * 100; // значение для преобразование в проценты
+
+      let thumb = this.elem.querySelector('.slider__thumb');
+      let progress = this.elem.querySelector('.slider__progress');
+
+      for (let step of allSteps) {
+        step.classList.remove('slider__step-active')
+      }
+
+      this.value = valuePercents / 100 * segmentSlider;
+
+      let customChange = new CustomEvent('slider-change', {
+        detail: this.value,
+        bubbles: true
+      });
+
+      slider.dispatchEvent(customChange);
+
+      allSteps[this.value].classList.add('slider__step-active');
+      sliderValue.innerHTML = `${this.value}`;
+      thumb.style.left = `${valuePercents}%`;
+      progress.style.width = `${valuePercents}%`;
+
+    });
+
+  }
+
+
 }
