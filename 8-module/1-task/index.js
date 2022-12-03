4fc47352e6b18878d1bccd57c5d719a9d9f5b2fa
Anton Protasov <antonprotasow69@gmail.com>
2022-12-03 19:53:08 +0100

    сделал корзину

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -39,13 +39,13 @@ export default class CartIcon {
   }
 
   updatePosition() {
-
+    let initialTopCoord = this.elem.getBoundingClientRect().top + window.pageYOffset;
     if (this.elem.offsetWidth && this.elem.offsetHeight) {
 
-      if (this.elem.getBoundingClientRect().top < 0) {
-
-        let leftIndent = Math.min(document.querySelector('.container').getBoundingClientRect().right + 20,
-          document.documentElement.clientWidth - this.elem.offsetWidth - 10) + 'px';
+      if (window.pageYOffset > initialTopCoord) {
+        let left = document.querySelector('.container').getBoundingClientRect().right + 20;
+        let right = document.documentElement.clientWidth - this.elem.offsetWidth - 10;
+        let leftIndent = Math.min(left, right) + 'px';
 
         Object.assign(this.elem.style, {
           position: 'fixed',
